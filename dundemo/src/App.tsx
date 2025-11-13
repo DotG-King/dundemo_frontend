8decf7b37cf3ca692efb3fd82086f454c141abcf
류준현 <tms02171@gmail.com>
2025-11-13 13:53:12 +0900

    init: 타입스크립트 기반 리엑트로 재생성

--- dundemo/src/App.tsx
+++ dundemo/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyNewComponents from './components/MyNewComponents'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,13 +22,12 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <MyNewComponents />
     </>
   )
 }
